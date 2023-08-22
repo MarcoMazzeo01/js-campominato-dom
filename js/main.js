560abe493abbbe74ad66b417db57430c08c47750
plastikSun <marcom2k01@gmail.com>
2023-08-22 16:01:23 +0200

    minor refactoring; swapped anonymous functions in addEventListener's

--- js/main.js
+++ js/main.js
@@ -6,14 +6,17 @@ let rootRules = document.querySelector(":root")
 
 let bombs = [];
 const maxBombs = 16;
+let score = 0
+let maxScore = 0;
 
+// ! start game here ! //
+startButton.addEventListener("click",startGame) 
 
-function RNG(min,max) {
-    return Math.floor((Math.random())*(max-min+1))+min;
-}
-
-startButton.addEventListener("click",function() {
+// * GAME FUNCTIONS * //
+function startGame() {
     grid.innerHTML = ""
+    score = 0
+
     let difficulty = selector.value
     rootRules.style.setProperty("--grid-divisor",selector.value)
 
@@ -36,20 +39,61 @@ startButton.addEventListener("click",function() {
     }
 
     bombs = bombsList(range)
+    maxScore = range - maxBombs
     console.table(bombs)
-})
+}
+
+function checkBomb(){
+    const num = parseInt(this.getAttribute("number"))
+
+    if (bombs.includes(num)) {
+        lose() 
+    } else {
+
+        if (this.getAttribute("selected") === "false") {
+            this.setAttribute("selected",true)
+            score++;
+            this.style.backgroundColor = "green"
+            this.style.color = "white"
+
+            if (score == maxScore) {
+                win()
+            }
+        }
+    }
+}
 
 
+function lose() {
+    //informa il giocatore del punteggio dopo aver perso e ricarica la pagina alla chiusura della finestra di dialogo
+    if (!alert("Hai perso! Punteggio: " + score)) {
+        window.location.reload()
+    }
+}
+
+function win() {
+    if (!alert("Congratulazioni! Hai vinto! Punteggio finale: " + score)) {
+        window.location.reload()
+    }
+}
+
+
+// * MISC * //
 function generateGrid(difficulty) {
     for (i = 1; i <= difficulty; i++) {
         let numLi = document.createElement("li")
         numLi.innerHTML = i
         numLi.setAttribute("number",i)
+        numLi.setAttribute("selected",false) //indica se la casella è già stata premuta dal giocatore
         grid.append(numLi)
 
-        numLi.addEventListener("click",function(){
-            console.log(this.getAttribute("number"))
-        })
+        numLi.addEventListener("click",checkBomb)
     }
     
 }
+
+function RNG(min,max) {
+    return Math.round((Math.random())*(max-min+1))+min;
+}
+
+
